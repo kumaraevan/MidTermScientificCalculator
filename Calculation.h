1cbfd5293756e398faccc94c8e1d7e9378bbf88c
kumaraevan <114914008+kumaraevan@users.noreply.github.com>
2022-10-12 14:27:00 +0700

    Update Calculation.h
    Signed-off-by: kumaraevan <114914008+kumaraevan@users.noreply.github.com>

--- Calculation.h
+++ Calculation.h
@@ -1,1 +1,26 @@
+#ifndef CALCULATION_H
+#define CALCULATION_H
+
+#include <string>
+using namespace std;
+
+/**
+ * Calculation node in linked list
+ */
+class Calculation
+{
+	public:
+		Calculation(string calc);
+		string get_current();
+		Calculation* get_next();
+		void set_next(Calculation* next);
+		
+	private:
+		string _data;
+		Calculation *_next;
+		
+	protected:
+};
+
+#endif
 
