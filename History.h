5c5dc260c9929af18b356c82ac12d6a380dedd84
kumaraevan <114914008+kumaraevan@users.noreply.github.com>
2022-10-12 15:20:11 +0700

    Update History.h
    Signed-off-by: kumaraevan <114914008+kumaraevan@users.noreply.github.com>

--- History.h
+++ History.h
@@ -1,1 +1,25 @@
+#ifndef HISTORY_H
+#define HISTORY_H
+
+#include <iostream>
+#include <string>
+#include "Calculation.h"
+
+class History
+{
+	public:
+		History();
+		Calculation* append(string calc);
+		int is_empty();
+		void clear();
+		void show();
+		
+	private:
+		Calculation* _head;
+		Calculation* _last;
+		
+	protected:
+};
+
+#endif
 
