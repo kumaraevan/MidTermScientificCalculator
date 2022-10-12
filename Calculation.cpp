2c20bd90598d254438fb391e4b06593a3f51f4de
kumaraevan <114914008+kumaraevan@users.noreply.github.com>
2022-10-12 14:17:34 +0700

    Update Calculation.cpp
    Signed-off-by: kumaraevan <114914008+kumaraevan@users.noreply.github.com>

--- Calculation.cpp
+++ Calculation.cpp
@@ -1,1 +1,23 @@
+#include "Calculation.h"
+
+Calculation::Calculation(string calc)
+{
+	_data = calc;
+	_next = NULL;
+}
+
+string Calculation::get_current()
+{
+	return _data;
+}
+
+Calculation* Calculation::get_next()
+{
+	return _next;
+}
+
+void Calculation::set_next(Calculation* next)
+{
+	_next = next;
+}
 
