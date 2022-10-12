1074b00a566966ca2fccee13d18a180223ac0463
kumaraevan <114914008+kumaraevan@users.noreply.github.com>
2022-10-12 13:39:55 +0700

    Update Calculator.cpp
    Signed-off-by: kumaraevan <114914008+kumaraevan@users.noreply.github.com>

--- Calculator.cpp
+++ Calculator.cpp
@@ -31,3 +31,57 @@ void Calculator::getoperand(float &op1, float &op2)
 	cin >> op2;
 	cout << endl;
 }
+
+void Calculator::getoperandtrig(float &op1)
+{
+	cout << "Please enter the angle (Degrees): ";
+	cin >> op1;
+	op1 = op1 * M_PI / 180;
+}
+
+void Calculator::getoneoperand(float &op1)
+{
+	cout << "Please enter the operand: ";
+	cin >> op1;
+	cout << endl;
+}
+
+
+//Math functions
+void Calculator::add(float op1, float op2)
+{
+	float result = op1 + op2;
+	
+	cout << op1 << " + " << op2 << " = " << result << endl << endl;
+	
+	stringstream stream;
+	stream.precision(3);
+	stream << op1 << " + " << op2 << " = " << result << endl;
+	
+	_hist->append(stream.str());
+}
+
+void Calculator::subtract(float op1, float op2)
+{
+	float result = op1 - op2;
+	cout << op1 << " - " << op2 << " = " << result << endl << endl;
+	
+	stringstream stream;
+	stream.precision(3);
+	stream << op1 << " - " << op2 << " = " << result << endl;
+	
+	_hist->append(stream.str());
+}
+
+void Calculator::multiply(float op1, float op2)
+{
+	float result = op1 * op2;
+	
+	cout << op1 << " * " << op2 << " = " << result << endl << endl;
+	
+	stringstream stream;
+	stream.precision(3);
+	stream << op1 << " * " << op2 << " = " << result << endl;
+	
+	_hist->append(stream.str());
+}
