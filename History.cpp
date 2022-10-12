84e2ecaf77e1af14e6be543da460ac3d46f3cd82
kumaraevan <114914008+kumaraevan@users.noreply.github.com>
2022-10-12 15:22:16 +0700

    Update History.cpp
    Signed-off-by: kumaraevan <114914008+kumaraevan@users.noreply.github.com>

--- History.cpp
+++ History.cpp
@@ -10,3 +10,37 @@ void History::clear()
 	_head = NULL;
 	_last = NULL;
 }
+
+Calculation* History::append(string calc)
+{
+	Calculation* new_node = new Calculation(calc);
+	
+	if(is_empty())
+		// First node
+		_head = new_node;
+	else
+		_last->set_next(new_node);
+	
+	_last = new_node;
+	return _last;
+}
+
+int History::is_empty()
+{
+	return _head == NULL;
+}
+
+void History::show()
+{
+	Calculation* cur = _head;
+	int i = 1;
+	
+	if(is_empty())
+		cout << "History is empty." << endl << endl;
+	else 
+		while(cur != NULL)
+		{
+			cout << i++ << ". " << cur->get_current() << endl;
+			cur = cur->get_next();
+		}
+}
